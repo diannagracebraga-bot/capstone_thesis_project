71655f17e7cfb6dc5509af6c189749d3e9cddb23
heartjoyabainza <heartjoy.abainza@gmail.com>
2026-08-02 14:44:48 +0800

    Revert "changess"
    This reverts commit e0cd39c3d6242d35db63a73102cd0c4a0d9440cc.

--- customer/customer_sidebar_header.php
+++ customer/customer_sidebar_header.php
@@ -1,24 +1,38 @@
-<nav class="navbar navbar-expand-lg navbar-dark">
+
+<nav class="navbar navbar-expand-lg navbar-dark ">
     <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"><span class="navbar-toggler-icon"></span></button>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>  </button>
+
         <div class="collapse navbar-collapse" id="navbarNav">
             <div class="dropdown ms-auto">
                 <button class="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown">
-                    <img src="" width="30" height="30" class="rounded-circle me-2">
-                    <?php echo htmlspecialchars($customer['first_name'] . ' ' . $customer['middle_name'] . ' ' . $customer['last_name']); ?>
-                </button>
-                <div class="dropdown-menu dropdown-menu-end"><a class="dropdown-item text-danger" href="../database/logout.php">Logout</a></div>
+
+                    <img src="" width="30" height="30"
+                      class="rounded-circle me-2">    <?php echo $customer['f_name'] . ' ' . $customer['m_name'] . ' ' . $customer['l_name']; ?> </button>
+
+                <div class="dropdown-menu dropdown-menu-end">
+                    <div>
+                        <a class="dropdown-item text-danger" href="../database/logout.php">
+                            Logout
+                        </a>
+</div>
+</div>
             </div>
         </div>
     </div>
 </nav>
 
+        
 <div class="sidebar">
-    <div class="logo"><img src="../images/bg_logo.png" alt="Logo" class="icon2"></div>
+    <div class="logo">
+        <img src="../images/bg_logo.png" alt="Logo" class="icon2">
+    </div>
     <a class="dashboard" href="customer-dashboard.php">Dashboard</a><br>
-    <a class="dashboard" href="customer_payment.php">Payment</a><br>
+    <a class="dashboard"href="customer_payment.php">Payment</a><br>
     <a class="dashboard" href="customer_support.php">Support</a><br>
     <a class="dashboard" href="customer_ticket.php">Ticket</a><br>
-    <a class="dashboard" href="customer_profile.php">Profile</a><br>
+    <a class="dashboard"href="customer_profile.php">Profile</a><br>
 </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
