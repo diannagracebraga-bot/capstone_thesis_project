73e8bb7da5d1580da57cfdfd87e2dfd5ab22fe56
leooncole-pixel <leooncole@gmail.com>
2026-08-09 10:43:04 +0800

    checl

--- admin/admin_add_admin.php
+++ admin/admin_add_admin.php
@@ -48,6 +48,7 @@ if (isset($_POST['submit'])) {
     <div class="card-body">
 <div class="container mt-4">
     <h2>Create Admin</h2>
+
     <form action="../admin/admin_user_management.php" method="POST">
         <div class="mb-3">
         </div>
